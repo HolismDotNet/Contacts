d1de15dee21398981b770f32b60892c524a57d4b
Saeed Nemati <sn.1361@gmail.com>
2022-01-25 10:14:10 +0330

    committed by script

--- DataAccess/ContactsContext.cs
+++ DataAccess/ContactsContext.cs
@@ -4,17 +4,17 @@ public class ContactsContext : DatabaseContext
 {
     public override string ConnectionStringName => "Contacts";
 
-    public DbSet<Address> Addresses { get; set; }
+    public DbSet<Contacts.Address> Addresses { get; set; }
 
-    public DbSet<AddressLocation> AddressLocations { get; set; }
+    public DbSet<Contacts.AddressLocation> AddressLocations { get; set; }
 
-    public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Contacts.Contact> Contacts { get; set; }
 
-    public DbSet<Phone> Phones { get; set; }
+    public DbSet<Contacts.Phone> Phones { get; set; }
 
-    public DbSet<Profile> Profiles { get; set; }
+    public DbSet<Contacts.Profile> Profiles { get; set; }
 
-    public DbSet<ProfileView> ProfileViews { get; set; }
+    public DbSet<Contacts.ProfileView> ProfileViews { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
